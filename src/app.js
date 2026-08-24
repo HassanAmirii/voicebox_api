7098a83a1de6e0026b1ba42029760562226f0be2
Hassan Amiri <hassanamiri.ai@gmail.com>
2026-08-24 21:47:35 +0100

    add dev url to cors

--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ const allowedOrigins =
   process.env.APP_ENV === "production"
     ? productionOrigin
     : [
+        "https://voicebox-dev.vercel.app",
         "http://localhost:3000",
         "http://localhost:5500",
         "http://localhost:5173",
