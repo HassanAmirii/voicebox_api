3c92824a8485d405a63afb9c7ed8eca145a894b2
Hassan Amiri <hassanamiri.ai@gmail.com>
2026-08-24 09:34:55 +0100

    add: mount deleteReport to router

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import {
+  deleteReport,
   getReport,
   patchReport,
   getStats,
@@ -16,7 +17,7 @@ router.patch("/reports/:id", auth, patchReport);
 router.get("/stats", auth, getStats);
 router.get("/admins", auth, getAllAdmins);
 router.delete("/admins/:username", auth, deleteAdmin);
+router.delete("/reports/:id", auth, deleteReport);
 router.post("/membership-code", auth, getMembershipCode);
 
 export default router;
-
